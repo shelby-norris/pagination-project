4f3ce7ef30d05840c3f35e8bf726dc33d0a083f0
Shelby Norris <shelbs9109@gmail.com>
2025-06-29 21:06:27 -0500

    corrected counter error and corrected next button disable feature by hard coding page limit vs using dynamic function, added setTimeout feature

--- pagination.js
+++ pagination.js
@@ -1,5 +1,6 @@
 const state = {
   currentPage: 1,
+  isRateLimit: false,
 };
 
 // Write a function that takes a page as a parameter and fetches the corresponding posts from the API
@@ -17,7 +18,6 @@ async function fetchPosts(page) {
     console.log(data);
 
     return data;
-
   } catch (error) {
     console.log("ERROR ", error.message);
   } finally {
@@ -29,14 +29,15 @@ fetchPosts(1);
 fetchPosts(2);
 
 // Write a function that dynamically creates and displays the posts on the page
-function renderPosts(postData) {
+function renderPosts(posts) {
   const postContainer = document.getElementById("post-container");
   postContainer.innerHTML = "";
   postContainer.className = "border-1 rounded p-4 border-indigo-400";
 
-  postData.forEach((post) => {
+  posts.forEach((post) => {
     const postElement = document.createElement("div");
-    postElement.className = "border-1 rounded p-4 border-indigo-400 text-indigo-800";
+    postElement.className =
+      "border-1 rounded p-4 border-indigo-400 text-indigo-800";
 
     const titleElement = document.createElement("p");
     titleElement.textContent = post.title;
@@ -52,11 +53,12 @@ function renderPosts(postData) {
   });
 }
 
-
 // Pagination Logic
 
-function renderPagination(id) {
-  const totalPages = Math.ceil(id / 10);
+function renderPagination(numFound) {
+  // const totalPages = Math.ceil(numFound / 10);
+
+  console.log(numFound, "data numFound");
 
   const paginationContainer = document.getElementById("pagination-container");
   paginationContainer.innerHTML = "";
@@ -68,28 +70,45 @@ function renderPagination(id) {
   prevButton.disabled = state.currentPage === 1;
   prevButton.onclick = async () => {
     state.currentPage--;
-    const data = await fetchPosts(state.currentPage);
+    if (state.isRateLimit === false) {
+      const data = await fetchPosts(state.currentPage);
+      state.isRateLimit = true;
 
-    renderPosts(data);
-    renderPagination(data);
+      console.log("DATA", data);
+      renderPosts(data);
+      renderPagination(data.numFound);
+    } else {
+      alert("SLOW DOWN!");
+      return;
+    }
+
+    setTimeout({}, (state.isRateLimit = false), 2000);
   };
 
   const nextButton = document.createElement("button");
   nextButton.textContent = "Next";
   nextButton.className =
     "border-2 rounded px-5 py-2 hover:cursor-pointer hover:bg-indigo-800 bg-indigo-200 text-indigo-800 hover:text-indigo-200";
-  nextButton.disabled = state.currentPage === totalPages;
+  nextButton.disabled = state.currentPage === 10;
   nextButton.onclick = async () => {
     state.currentPage++;
-    const data = await fetchPosts(state.currentPage);
+    if (state.isRateLimit === false) {
+      const data = await fetchPosts(state.currentPage);
+      state.isRateLimit = true;
 
-    renderPosts(data);
-    renderPagination(data);
+      renderPosts(data);
+      renderPagination(data.numFound);
+    } else {
+      alert("SLOW DOWN!");
+      return;
+    }
+
+    setTimeout({}, (state.isRateLimit = false), 2000);
   };
 
   const pageCounter = document.createElement("p");
   pageCounter.className = "text-indigo-800";
-  pageCounter.textContent = `Page ${state.currentPage} of ${totalPages}`;
+  pageCounter.textContent = `Page ${state.currentPage} of 10`;
 
   paginationContainer.appendChild(prevButton);
   paginationContainer.appendChild(pageCounter);
@@ -106,5 +125,5 @@ async function handleDisplay(event) {
   const data = await fetchPosts(state.currentPage);
   console.log("Fetched Posts: ", data);
   renderPosts(data);
-  renderPagination(data);
+  renderPagination(data.numFound);
 }
